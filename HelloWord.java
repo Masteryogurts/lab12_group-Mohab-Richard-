0b89032fd41f74a62e7ce0579bf9b0495b053f4f
Webb_Richard <r.webb1@gwmail.gwu.edu>
2025-11-05 19:08:16 -0500

    in a branch my version

--- HelloWord.java
+++ HelloWord.java
@@ -1,5 +1,8 @@
 public class HelloWord {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("Richard Webb");
+        for (int i = 0; i < 2; i++){
+            System.out.println("This will print TWICE");
+        }
     }
 }
