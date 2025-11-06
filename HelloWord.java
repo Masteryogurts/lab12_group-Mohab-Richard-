7aba0e8569ad727108e6b600d8a97e46d81c3439
Webb_Richard <r.webb1@gwmail.gwu.edu>
2025-11-05 19:06:13 -0500

    changes

--- HelloWord.java
+++ HelloWord.java
@@ -1,6 +1,8 @@
 public class HelloWord {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
-        System.out.println("me");
+        System.out.println("Richard webb");
+        for (int i = 0; i < 2; i++){
+            System.out.println("this will print twic");
+        }
     }
 }
